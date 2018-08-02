e9cd670fcd2540dcbb221ceb743b996d3ae2a864
Ahmed Bilal <ahmedbilal96@gmail.com>
2018-08-02 18:07:29 +0500

    DB added, Documentation added in README.md, Update template

---
+++ README.md
@@ -0,0 +1,20 @@
+# Flask Tutorial Documentation
+
+**Technology Used:** Flask (Python 3.5)
+
+**Database Used:** SQLite 3
+
+**ORM Used:** SQLAlchemy
+
+**CSS Framework:** Spectre
+
+**Code Highlighter:** highlight.js
+
+----------------------
+## **URL: /**
+----------------------
+**Methods:** GET
+
+**Arguments:** None
+
+**Description:** Returns all code snippets in database
--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pastes.db'
+db = SQLAlchemy(app)
+
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+Pastes = Base.classes.Pastes
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    result = db.session.query(Pastes).all()
+    return render_template('base.html', result=result)
 
 
 if __name__ == '__main__':
